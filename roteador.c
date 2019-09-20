828fb7139e35d3b9a6a053762d5005f096756ba6
vitor-apolinario <vitorapoli@gmail.com>
2019-09-20 11:41:41 -0300

    envio e recebimento de mensagens utilizando somente um socket

--- roteador.c
+++ roteador.c
@@ -30,11 +30,19 @@ struct roteador
 struct roteador *roteadores;
 int caminho[NODES][NODES], adjacencia[NODES][NODES];
 int n_adj=1;   //n_adj começa com um porque já considera sua própria porta
+int sock;
+struct sockaddr_in si_me, si_other;
 
 void die(char *s)
 {
     perror(s);
     exit(1);
+}
+
+void printRoteadores(){
+    puts("Roteador atual e vizinhos:");
+    for(int i=0; i<n_adj; i++)
+        printf("roteador:%d | porta:%d\n", roteadores[i].id, roteadores[i].porta);
 }
 
 int main(void)
@@ -62,14 +70,16 @@ int main(void)
 
     pthread_t tids[2];
 
-    puts("Roteador atual e vizinhos:");
-    for(int i=0; i<n_adj; i++)
-        printf("roteador:%d | porta:%d\n", roteadores[i].id, roteadores[i].porta);
+    // printRoteadores();
+
+    bindMySocket();    
 
     pthread_create(&tids[0], NULL, receiver, (void *) &roteadores[0].porta);
     pthread_create(&tids[1], NULL, sender, NULL);
     pthread_join(tids[0], NULL);
     pthread_join(tids[1], NULL);
+
+    close(sock);
 
    return(1);
 }
@@ -161,21 +171,34 @@ void carregaConfigs(int adjacentes[])
     }
 }
 
+void bindMySocket()
+{
+    //create a UDP socket
+    if ((sock=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    {
+        die("socket");
+    }
+
+    memset((char *) &si_me, 0, sizeof(si_me));
+    si_me.sin_family = AF_INET;
+    si_me.sin_port = htons(roteadores[0].porta);
+    si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+     
+    if( bind(sock , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1)
+    {
+        die("bind");
+    }
+}
+
 void *sender() 
 {
-    struct sockaddr_in si_other;
-    int s, i, slen=sizeof(si_other);
+    int i, slen=sizeof(si_other);
     char buf[BUFLEN];
     char packet[BUFLEN], message[BUFLEN];
     int id_destino;
  
     while(1)
     {
-        if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
-        {
-            die("socket");
-        }
-
         printf("Enter router id:\n");
         scanf("%d", &id_destino);
         
@@ -186,13 +209,6 @@ void *sender()
         scanf("%s", message);
     
         strcat(packet, message);
-
-        /*
-        if(i==n_adj){
-            puts("o roteador destino não é acessível");
-            continue;
-        }
-        */
 
         int id_next = caminho[roteadores[0].id][id_destino];
 
@@ -214,11 +230,10 @@ void *sender()
             exit(1);
         }
          
-        if (sendto(s, packet, strlen(packet) , 0 , (struct sockaddr *) &si_other, slen)==-1)
+        if (sendto(sock, packet, strlen(packet) , 0 , (struct sockaddr *) &si_other, slen)==-1)
         {
             die("sendto()");
         }
-        close(s);
     }
 
     return 0;
@@ -226,44 +241,17 @@ void *sender()
 
 void *receiver(void *porta) 
 {
-    struct sockaddr_in si_me, si_other;
-     
-    int s, i, slen = sizeof(si_other) , recv_len;
+    int i, slen = sizeof(si_other) , recv_len;
     char buf[BUFLEN];
-    int port = *((int*) porta);
     char *token;
     int id_destino = -1;
 
-    printf("Escutando na porta  %d\n", port);
-     
-    //create a UDP socket
-    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
-    {
-        die("socket");
-    }
-     
-    // zero out the structure
-    memset((char *) &si_me, 0, sizeof(si_me));
-     
-    si_me.sin_family = AF_INET;
-    si_me.sin_port = htons(port);
-    si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-     
-    //bind socket to port
-    if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1)
-    {
-        die("bind");
-    }
-     
-    //keep listening for data
     while(1)
     {
-        //receive a reply and print it
-        //clear the buffer by filling null, it might have previously received data
         memset(buf,'\0', BUFLEN);
 
         //try to receive some data, this is a blocking call
-        if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1)
+        if ((recv_len = recvfrom(sock, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1)
         {
             die("recvfrom()");
         }
@@ -293,7 +281,7 @@ void *receiver(void *porta)
                 exit(1);
             }
             
-            if (sendto(s, copy, strlen(copy) , 0 , (struct sockaddr *) &si_other, slen)==-1)
+            if (sendto(sock, copy, strlen(copy) , 0 , (struct sockaddr *) &si_other, slen)==-1)
             {
                 die("sendto()");
             }
@@ -304,6 +292,5 @@ void *receiver(void *porta)
         }
     }
  
-    close(s);
     return 0;
 }
