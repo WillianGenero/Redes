c236163b3c27cf09eacdd27701298a3ffd3bf69f
vitor-apolinario <vitorapoli@gmail.com>
2019-09-28 13:18:44 -0300

    mudada estrutura do pacote, agora acontece confirmação de entrega do pacote, mudada estrutura de arquivos

---
+++ headers/structures.h
@@ -0,0 +1,23 @@
+#define BUFLEN 100
+#define NODES 6
+
+// packet types
+#define DATA 0
+#define CONTROL 1
+
+typedef struct roteador
+{
+    int id;
+    int porta;
+    char ip[32];
+} roteador;
+
+typedef struct pacote
+{
+    int id_dest;
+    int id_font;
+    int seq;
+    char message[150];
+    int type;
+    int ack;
+} pacote;
--- roteador.c
+++ roteador.c
@@ -5,13 +5,7 @@
 #include<sys/socket.h>
 #include<pthread.h>
 #include<unistd.h>
-
-#define NODES 6
-
-#define BUFLEN 100
-
-#define SEND 0
-#define ROUTE 1
+#include "headers/structures.h"
 
 int searchMinor();
 void savePath();
@@ -19,31 +13,19 @@ void dijkstra();
 
 void loadLinks();
 void loadConfs(int adjacentes[]);
-
 void socketConfig();
-void sendMessage(int id, char message[], int mode);
 
-void *prompt();
+void sendPacket(pacote packet);
+
+void *terminal();
 void *router(void *porta);
 
-struct roteador
-{
-    int id;
-    int porta;
-    char ip[32];
-};
-
-struct pacote
-{
-    int id;
-    char message[BUFLEN];
-};
-
 struct roteador *roteadores;
-int caminho[NODES][NODES], adjacencia[NODES][NODES];
-int n_adj=1;   //n_adj começa com um porque já considera sua própria porta
-int sock;
 struct sockaddr_in si_me, si_other;
+int caminho[NODES][NODES], adjacencia[NODES][NODES];
+int n_adj=1;
+int sock;
+int seq = 0;
 
 void die(char *s)
 {
@@ -52,9 +34,11 @@ void die(char *s)
 }
 
 void printRoteadores(){
-    puts("Roteador atual e vizinhos:");
-    for(int i=0; i<n_adj; i++)
-        printf("roteador:%d %s:%d\n", roteadores[i].id, roteadores[i].ip, roteadores[i].porta);
+    puts("---------------------------------");
+    printf("| id:%d %s:%d\n| Vizinhos:\n", roteadores[0].id, roteadores[0].ip, roteadores[0].porta);
+    for(int i=1; i<n_adj; i++)
+        printf("| id:%d | %s:%d\n", roteadores[i].id, roteadores[i].ip, roteadores[i].porta);
+    puts("---------------------------------");
 }
 
 int main(void)
@@ -87,7 +71,7 @@ int main(void)
     socketConfig();    
 
     pthread_create(&tids[0], NULL, router, (void *) &roteadores[0].porta);
-    pthread_create(&tids[1], NULL, prompt, NULL);
+    pthread_create(&tids[1], NULL, terminal, NULL);
     pthread_join(tids[0], NULL);
     pthread_join(tids[1], NULL);
 
@@ -150,7 +134,7 @@ void loadLinks(){
 
    memset(adjacencia, 0, sizeof(adjacencia));
 
-    FILE *file = fopen("enlaces.config", "r");
+    FILE *file = fopen("configs/enlaces.config", "r");
     if (!file)
         die("Não foi possível abrir o arquivo de Enlaces");
 
@@ -170,7 +154,7 @@ void loadConfs(int adjacentes[])
     roteadores = malloc(sizeof(struct roteador) * n_adj);
 
     for(int i=0; i<n_adj; i++){
-        file = fopen("roteador.config", "r");
+        file = fopen("configs/roteador.config", "r");
         if (!file)
             die("Não foi possível abrir o arquivo de Roteadores");
 
@@ -204,26 +188,18 @@ void socketConfig()
     }
 }
 
-void sendMessage(int id, char message[], int mode)
+void sendPacket(pacote packet)
 {
-    // char packet[BUFLEN];
     int id_next, i, slen=sizeof(si_other);
-    struct pacote pacote;
-    
-    if(mode == SEND)
-        pacote.id = id;
 
-    strcpy(pacote.message, message);
-
-    id_next = caminho[roteadores[0].id][id];
+    id_next = caminho[roteadores[0].id][packet.id_dest];
 
     for(i=1; i<n_adj; i++){
         if (roteadores[i].id == id_next)
             break;
     }
 
-    printf("route destino %d\n", id_next);
-    printf("porta destino %d\n", roteadores[i].porta);
+    printf("...encaminhando via roteador: %d | %s:%d\n", id_next, roteadores[i].ip, roteadores[i].porta);
 
     memset((char *) &si_other, 0, sizeof(si_other));
     si_other.sin_family = AF_INET;
@@ -235,28 +211,32 @@ void sendMessage(int id, char message[], int mode)
         exit(1);
     }
         
-    if (sendto(sock, &pacote, sizeof(struct pacote) , 0 , (struct sockaddr *) &si_other, slen)==-1)
+    if (sendto(sock, &packet, sizeof(struct pacote) , 0 , (struct sockaddr *) &si_other, slen)==-1)
     {
         die("sendto()");
     }
 }
 
-void *prompt() 
+void *terminal() 
 {
     int i, slen=sizeof(si_other);
-    char buf[BUFLEN];
-    char packet[BUFLEN], message[BUFLEN];
-    int id_destino;
- 
+    pacote packet;
+
     while(1)
     {
         printf("Enter router id:\n");
-        scanf("%d", &id_destino);
+        scanf("%d", &packet.id_dest);
         
         printf("Enter message:\n");
-        scanf("%s", message);
+        scanf("%s", packet.message);
 
-        sendMessage(id_destino, message, SEND);
+        
+        packet.seq = ++seq;
+        packet.type = DATA;
+        packet.ack = 0;
+        packet.id_font = roteadores[0].id;
+
+        sendPacket(packet);
     }
 
     return 0;
@@ -265,33 +245,47 @@ void *prompt()
 void *router(void *porta) 
 {
     int i, slen = sizeof(si_other) , recv_len;
-    char buf[BUFLEN];
-    char *token;
     int id_destino = -1;
-    struct pacote pacote;
+    pacote packet;
 
     while(1)
     {
-        memset(buf,'\0', BUFLEN);
-
         //try to receive some data, this is a blocking call
-        if ((recv_len = recvfrom(sock, &pacote, sizeof(struct pacote), 0, (struct sockaddr *) &si_other, &slen)) == -1)
+        if ((recv_len = recvfrom(sock, &packet, sizeof(struct pacote), 0, (struct sockaddr *) &si_other, &slen)) == -1)
         {
             die("recvfrom()");
         }
         
-        id_destino = pacote.id;
+        id_destino = packet.id_dest;
 
+        sleep(1);
         if (id_destino != roteadores[0].id){
             int id_next = caminho[roteadores[0].id][id_destino];
-            sleep(1);
-            printf("Pacote para %d encaminhando por %d...\n", id_destino, id_next);
-            sendMessage(id_next, pacote.message, ROUTE);            
+            
+            if(packet.type == DATA){
+                printf("Roteador %d encaminhando mensagem com # sequência %d para o destino %d\n", roteadores[0].id, packet.seq, packet.id_dest);
+            }else if (packet.type == CONTROL && packet.ack == 1){
+                printf("Roteador %d encaminhando confirmação de msg #seq:%d para o sender %d\n", roteadores[0].id, packet.seq, packet.id_dest);
+            }
+            sendPacket(packet);
 
-        }else{
-            sleep(1);
+        }else if(id_destino == roteadores[0].id && packet.type == DATA){
+            pacote response;
+            response.type = CONTROL;
+            response.ack = 1;
+            response.id_font = id_destino;
+            response.id_dest = packet.id_font;
+            response.seq = packet.seq; // ou ++seq ?
+
             printf("Pacote recebido de %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-            printf("Mensagem: %s\n", pacote.message);
+            printf("Mensagem: %s\n", packet.message);
+            sleep(1);
+            puts("Enviando confirmação...");
+            
+            sendPacket(response);
+            
+        }else if(id_destino == roteadores[0].id && packet.ack == 1){
+            printf("Confirmação recebida de %s:%d, mensagem #seq:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port), packet.seq);
         }
     }
  
